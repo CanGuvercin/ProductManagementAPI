fb22d9b1e32ecef45cfbae2ceee32a5a6127d20b
Can Guvercin <mustafacanguvercin@gmail.com>
2025-05-13 22:51:17 +0300

    Project structure initialized, model created with product class

---
+++ src/main/java/eu/guvercin/model/Product.java
@@ -0,0 +1,14 @@
+package eu.guvercin.model;
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Product {
+    private Long id;
+    private String name;
+    private Double price;
+
+}
---
+++ src/main/java/eu/guvercin/service/ProductService.java
@@ -0,0 +1,4 @@
+package eu.guvercin.service;
+
+public class ProductService {
+}
